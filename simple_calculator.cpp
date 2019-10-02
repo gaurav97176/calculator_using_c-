c1e3b1cf33730c6bf78d88356ff9ca7f53049a5d
gaurav97176 <44135243+gaurav97176@users.noreply.github.com>
2019-10-02 09:44:31 +0530

    added 'endl' to improve viewablity

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -4,9 +4,9 @@ using namespace std;
 //simple operations like addition,multiplication,subtraction and division..
 int main(){
     double a,b;char c;
-    cout<<"\n enter two no's";
+    cout<<"\n enter two no's"<<endl;
     cin>>a>>b;
-    cout<<"\n what operation you want to perform:";
+    cout<<"\n what operation you want to perform:"<<endl;
     cin>>c;
     if(c=='+') cout<<a+b<<endl;
     else if(c=='-') cout<<a-b<<endl;
